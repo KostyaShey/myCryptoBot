c653b4cda6a9261a4dcc7a0b6d5f32c16b4d2625
Konstantin Sheynkler <k.sheynkler@globit.com>
2021-05-10 11:57:11 +0200

    little refactoring of init

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,6 +3,11 @@ from flask import (Flask, g, request, redirect, url_for)
 from flask import render_template
 from flaskr.db import get_db
 import time
+from . import db
+from . import coinmarketcapAPI_test
+from . import telegram_notifier
+from . import coinmarketcap_api
+
 
 def create_app(test_config=None):
     # create and configure the app
@@ -25,14 +30,11 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    from . import db
     db.init_app(app)
 
-    from . import coinmarketcapAPI_test
     app.register_blueprint(coinmarketcapAPI_test.bp)
-
-    from . import telegram_notifier
     app.register_blueprint(telegram_notifier.bp)
+    app.register_blueprint(coinmarketcap_api.bp)
 
     @app.route('/')
     def hello():
@@ -47,10 +49,7 @@ def create_app(test_config=None):
         if request.method == 'POST':
             get_db().execute(
                 f'UPDATE config SET low_24h = {request.form["low_24h"]}, high_24h = {request.form["high_24h"]}, low_7d = {request.form["low_7d"]}, high_7d = {request.form["high_7d"]} WHERE id = 1')
-            get_db().commit()      
+            get_db().commit()
         return redirect('/')
-
-    from . import coinmarketcap_api
-    app.register_blueprint(coinmarketcap_api.bp)
 
     return app
